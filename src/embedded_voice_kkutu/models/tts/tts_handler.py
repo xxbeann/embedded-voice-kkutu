beaa0860a1ac14bab35c00b035df71c8b69af3ef
xxbeann <yjb4945@gmail.com>
2024-12-14 08:36:25 +0900

    fix: pygame 라이브러리 사용해서 다시 구현
    playsound라이브러리는 플랫폼에 따라 다르게 동작합니다. 따라서 pygame
    라이브러리를 적용하여 다시 개발했습니다.

--- src/embedded_voice_kkutu/models/tts/tts_handler.py
+++ src/embedded_voice_kkutu/models/tts/tts_handler.py
@@ -1,6 +1,6 @@
 import os
 from gtts import gTTS
-from playsound import playsound
+import pygame
 
 
 # 텍스트를 음성으로 변환(TTS)하고 재생하는 핸들러 클래스
@@ -9,11 +9,13 @@ class TTSHandler:
     # TTS 핸들러 초기화(생성자)
     def __init__(self, lang="ko"):
         self.lang = lang
+        pygame.mixer.init()  # Pygame 오디오 초기화
 
     # 언어 설정 변경 메서드
     def set_language(self, lang: str):
         self.lang = lang
 
+    # 텍스트를 음성으로 변환하고 재생하는 메서드
     def text_to_speech(self, text: str):
         if not text.strip():
             raise ValueError("텍스트가 비어있습니다. 음성을 생성할 수 없습니다.")
@@ -24,7 +26,12 @@ class TTSHandler:
         tts.save(temp_file)
 
         # MP3 파일 재생
-        playsound(temp_file)
+        pygame.mixer.music.load(temp_file)
+        pygame.mixer.music.play()
+
+        # 오디오가 끝날 때까지 대기
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
 
         # 임시 파일 삭제
         os.remove(temp_file)
--- tests/models/test_tts_handler.py
+++ tests/models/test_tts_handler.py
@@ -1,12 +1,12 @@
 from embedded_voice_kkutu.models.tts import TTSHandler
 
-
+# 테스트 코드
 def main():
     # TTSHandler 객체 생성 (기본 언어: 한국어)
     tts_handler = TTSHandler()
 
-    # 텍스트를 음성으로 출력
-    tts_handler.text_to_speech("안녕하세요. TTS 핸들러입니다.")
+    # # 텍스트를 음성으로 출력
+    # tts_handler.text_to_speech("안녕하세요. TTS 핸들러입니다.")
 
     # 언어를 영어로 변경 후 테스트
     tts_handler.set_language("en")
