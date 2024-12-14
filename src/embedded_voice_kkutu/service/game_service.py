0145bc54d66ecfaf2bb83f9822905bedb62e1dcb
Jonghyeon Park <belline0124@gmail.com>
2024-12-14 23:55:04 +0900

    feat: apply init law to game

--- src/embedded_voice_kkutu/service/game_service.py
+++ src/embedded_voice_kkutu/service/game_service.py
@@ -1,6 +1,7 @@
 import json
 from typing import Dict, List, Set, Optional
-
+from embedded_voice_kkutu.models.ko_kr import KoKrSupport
+from random import sample
 
 def load_word_dictionary() -> Dict[str, List[str]]:
     try:
@@ -15,14 +16,21 @@ def load_word_dictionary() -> Dict[str, List[str]]:
 class WordChainGame:
     def __init__(self):
         self.words_dict = load_word_dictionary()
-        self.used_words: Set[str] = set()  # 이미 사용된 단어 저장
+        self.used_words: set[str] = set()  # 이미 사용된 단어 저장
 
     def is_valid_word(self, current_word: Optional[str], word: str) -> bool:
         # 첫 단어가 아닐 경우 이전 단어 마지막 글자와 입력 첫 글자가 같은 지 확인
         if current_word is not None:
-            if current_word[-1] != word[0]:
+            ables = [current_word[-1]]
+            init_law_applied = KoKrSupport.init_law_char(current_word[-1])
+            if init_law_applied != current_word[-1]:
+                ables.append(init_law_applied)
+            
+            if word[0] not in ables:
                 print(
-                    f"이전 단어의 마지막 글자 '{current_word[-1]}'로 시작해야 합니다."
+                    "이전 단어의 마지막 글자 {}로 시작해야 합니다.".format(
+                        ables[0] if len(ables) == 1 else f"{ables[0]} 또는 {', '.join(ables[1:])}"
+                    )
                 )
                 return False
 
@@ -39,11 +47,28 @@ class WordChainGame:
         return True
 
     def get_next_word(self, last_letter: str) -> str:
-        possible_words = self.words_dict.get(last_letter, [])
-        for word in possible_words:
+        possible_words = set()
+        if last_letter in self.words_dict:
+            possible_words = possible_words.union(set(self.words_dict[last_letter]))
+        init_law_applied = KoKrSupport.init_law_char(last_letter)
+        if init_law_applied != last_letter and init_law_applied in self.words_dict:
+            possible_words = possible_words.union(set(self.words_dict[init_law_applied]))
+
+        remains = len(possible_words)
+        rejected = set()
+        
+        while remains > len(rejected):
+            word = sample(possible_words, 1)
+            if len(word) == 0:
+                break
+            word = word[0]
+
             if word not in self.used_words:
                 self.used_words.add(word)
                 return word
+            else:
+                rejected.add(word)
+
         return None
 
     def play_turn(self, player_word: str) -> str:
